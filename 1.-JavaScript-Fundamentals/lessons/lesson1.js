9fb987501025bbd758b42a196178308d1f9a01b7
Jairo Arturo Ledezma Garcia <jairoarturoledezmagarcia@Jairos-MacBook-Air.local>
2026-08-19 18:23:16 -0600

    first commit

---
+++ 1.-JavaScript-Fundamentals/lessons/lesson1.js
@@ -0,0 +1,38 @@
+//1.- hello world
+
+console.log("Hello World");
+
+// Variables
+var firstName = "Jairo";
+let lastName = "Ledezma";
+
+console.log("my name is " + firstName + " " + lastName);
+
+var age, dateOfBirth, sex;
+
+age = 5;
+dateOfBirth = "Jan";
+sex = "Male";
+
+console.log(age);
+
+age = 10;
+
+console.log(age);
+
+// constants
+
+const occupation = "Engineer";
+console.log(occupation);
+
+//occupation = "test engineer" // throws error constants cant change its value
+console.log(occupation);
+
+//const test; // a constant needs to be declared 
+
+//data types 
+var middleName = "Arturo" // string 
+var ageOfBrother = 25 // number
+var isHeMarried = false // boolean can have only 2 values true or false 
+var yearsInMarriage = null // null represents the absence of a value, not a specific value itself.
+var numberOfCars = undefined //A value has not been assigned yet.
