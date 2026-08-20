3599db45dafb741d9390f4c59db31afe6cfca900
Jairo Arturo Ledezma Garcia <jairoarturoledezmagarcia@Jairos-MacBook-Air.local>
2026-08-19 18:38:02 -0600

    added file with concatenation and interpolation examples

---
+++ 1.-JavaScript-Fundamentals/lessons/lesson2.js
@@ -0,0 +1,16 @@
+//concatenation and interpolation
+
+var product = "cup"
+var price = 50 
+var messageToPrint = "The price for your " + product + " is " + price + " dollars" // concatenation 
+
+console.log(messageToPrint)
+
+
+// interpolation 
+
+var product2 = "gear"
+var price2 = 500 
+var messageToPrint2 = `the price for your ${product2} is ${price2} dollars` // interpolation 
+
+console.log(messageToPrint2)
